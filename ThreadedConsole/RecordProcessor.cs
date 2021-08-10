c4f474289209f610642412150230a70046d34388
Sean <srafferty89@gmail.com>
2021-08-10 21:55:49 +0100

    new planned record handler!

---
+++ ThreadedConsole/RecordProcessor.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using ThreadedConsole.Models;
+
+namespace ThreadedConsole
+{
+    public class RecordProcessor
+    {
+        public RecordProcessor()
+        {
+
+        }
+
+        public async void Process(List<CSVRecord> records)
+        {
+
+        }
+    }
+}
