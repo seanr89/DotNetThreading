acdc9f8bcd4e61ccde553f1c5a29d841eef3edca
Sean <srafferty89@gmail.com>
2021-10-29 22:31:03 +0100

    new sln added and program update!

--- ParallelConsole/Program.cs
+++ ParallelConsole/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace ParallelConsole;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+
+    }
+
+    private IHostBuilder CreateHostBuilder(string[] args)
+    {
+
+    }
+}
